a78d41f6684342c1847a85bf2b19f6257af484d4
JakubPawlicki <100231154+JakubPawlicki@users.noreply.github.com>
2024-07-20 12:10:34 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="module")
 def browser():
-    # options = Options()
-    # options.add_argument('--headless')
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--disable-dev-shm-usage')
-    # # options.binary_location = "/opt/google/chrome/google-chrome"
-    # service = ChromeService(ChromeDriverManager().install(), options=options)
-    driver = webdriver.Chrome(executable_path="/opt/hostedtoolcache/setup-chrome/chromium/1330686/x64")
+    chrome_options = Options()
+    chrome_options.binary_location = "/opt/hostedtoolcache/setup-chrome/chromium/1330686/x64"
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(5)
     driver.get("https://victorious-moss-09004da03.5.azurestaticapps.net/")
     yield driver
